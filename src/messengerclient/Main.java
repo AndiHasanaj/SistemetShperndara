30863501074502a764135ceb6c267279e14d6baf
AlbinaAhmeti <albinaahmeti6@gmail.com>
2022-05-23 22:10:19 +0200

    Chat

---
+++ src/messengerclient/Main.java
@@ -0,0 +1,20 @@
+package messengerclient;
+
+import messengerclient.ClientManager;
+import messengerclient.LoginFrame;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        ClientManager clientManager=new ClientManager();
+        LoginFrame loginFrame=new LoginFrame(clientManager);
+        loginFrame.setLocation(400,100);
+        loginFrame.setVisible(true);
+        // TODO code application logic here
+    }
+
+}
